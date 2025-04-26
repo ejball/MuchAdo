7adfc366888089066708b4c5ca02348931c25561
Ed Ball <ed.ball@logos.com>
2025-04-25 19:13:11 -0700

    WIP

--- src/MuchAdo.MySql/MySqlDbDataMapper.cs
+++ src/MuchAdo.MySql/MySqlDbDataMapper.cs
@@ -2,5 +2,7 @@ namespace MuchAdo.MySql;
 
 public static class MySqlDbDataMapper
 {
-	public static DbDataMapper Default { get; } = new(new DefaultDbTypeMapperFactory(), new MySqlDbTypeMapperFactory());
+	public static DbDataMapper Default { get; } = new(
+		new DefaultDbTypeMapperFactory(),
+		new MySqlDbTypeMapperFactory());
 }
